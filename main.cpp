aa4782b02249c9329e8094ad7ced943bddb3482e
leoleleo <leo.mathis13@gmail.com>
2016-06-08 22:21:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,16 +12,32 @@ https://projecteuler.net/problem=317
 
 #include "parametres.h"
 
-//this function will return f(x)/f'(x) where f(x)=x+sqrt(x^2+ax)-1 the zero of f gives us sin(theta)^2
+
+//the zero of f gives us sin(theta)^2
+double f(double x, double a){
+  return x+sqrt(x*x+x*a)-1;
+}
+
+//this function will return f(x)/f'(x) used in newton's method
 double newton_ratio(double x, double a) {
   double b=sqrt(x*x+a*x);
   return b*(b+x-1)/(b+1+a/2);
 }
 
+//here we iterate newton's method to find the zero of f
+double newton_suite(double a){
+  double x=a;
+  while (f(x, a)*f(x, a)>=PREC){
+    x=x-newton_ratio(x, a);
+  }
+  return x;
+}
 
 
 
 main{int argc, char ** argv) {
+  
+  
   
   return 0;
 }
