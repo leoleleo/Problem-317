5d59099716515f91fef60923f2c5e48761217688
leoleleo <leo.mathis13@gmail.com>
2016-06-09 11:11:33 +0200

    Update parametres.h

--- parametres.h
+++ parametres.h
@@ -1,4 +1,5 @@
-#define GRAV 9.81
-#define ZO 100
-#define VO 20
-#define PREC 0.00001
+#define   GRAV    9.81
+#define   ZO      100
+#define   VO      20
+#define   PREC    0.00001
+#define   PI      3.14159265358979323846
